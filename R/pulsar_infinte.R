31c6fa1e91becb544ad23fda344ac765ac14c255
Tanushree Dalvi <tanushreedalvi72@gmail.com>
2025-08-15 23:05:50 +0100

    Add files via upload

---
+++ R/pulsar_infinte.R
@@ -0,0 +1,68 @@
+
+#' InfInte function for pulsar
+#' Function to be used by pulsar to perform the abduction from a subset of the OTU table and obtain a list of adjacency matrix along the lambda path
+#' @param sub_otu_tb subset of the OTU table provided by pulsar
+#' @param lambda numeric vector containing the lambda path obtained with the \code{\link{getLamPath}} function.
+#' @param bottom_clauses list including a python dictionary with the bottom clauses together with all the information required for the abduction.
+#' @param hypothesis character vector where each element relates the different observations produced by the get_observation functions.
+#' @param exclusion logical indicating if exclusion cases have to be considered for generating the logic clauses. Default is FALSE.
+#' @param otu_data list joining all the OTU information. Obtained using the join_abundances function.
+#'
+#' @return a list of p∗p adjacency matrices, one for each value of lambda.
+#' @export
+#'
+#' @examples
+#' pulsar_infinte(sub_otu_tb, lambda, bottom_clauses, hypothesis, exclusion, otu_data)
+pulsar_infinte <- function(sub_otu_tb, lambda, bottom_clauses, hypothesis, exclusion, otu_data) {
+  sub_otu_tb <- t(sub_otu_tb)
+  # Get head clauses from sub sampled table
+
+  comparisons <- get_comparsions(ncol(sub_otu_tb))
+  sub_depth <- otu_data$depth[colnames(sub_otu_tb)]
+
+  # Head
+  head_clauses <- lapply(rownames(sub_otu_tb), function(otu) {
+    pos <- which(rownames(sub_otu_tb) == otu)
+    abundances <- do.call(
+      what = otu_data$abundance_function[pos],
+      args = list(
+        "otu_abundance" = sub_otu_tb[pos, , drop = FALSE],
+        "comparisons" = comparisons, "depth" = sub_depth, "exclusion" = exclusion
+      )
+    )
+    return(abundances)
+  })
+  head_clauses <- unlist(head_clauses)
+
+  # Abduce
+  abduced <- abduce(bottom_clauses =  bottom_clauses, hypothesis = hypothesis, head_clauses = head_clauses)
+
+  # Get I values
+  abduced.table <- get_I_values(abduced)
+
+  snames <- rownames(otu_data$otu_tb)
+  
+  # Add non interacting asvs
+  noi.asvs <- snames[!snames %in% unique(c(abduced.table$sp1, abduced.table$sp2))]
+  noi.tab <- data.frame(noi.asvs, noi.asvs, rep("no_effect",
+                                        length(noi.asvs)), rep(0, length(noi.asvs)))
+  colnames(noi.tab) <- colnames(abduced.table)
+  abduced.table <- rbind(abduced.table, noi.tab)
+  
+  # Construct adjacency matrix
+  g <- igraph::graph_from_data_frame(abduced.table[, c(1, 2, 4)])
+  ad <- igraph::as_adj(g, attr = "comp")
+  #ad[upper.tri(ad)]<- ad[lower.tri(ad)]
+  
+  # For each lambda
+  pt <- lapply(lambda, function(lam) {
+    # Compression bigger than lambda
+    tmp <- ad > lam
+    diag(tmp) <- FALSE
+    
+    tmp<- as(tmp, "lMatrix")
+    return(tmp)
+  })
+  # Return the path over lambda
+  return(list(path = pt))
+}
---
+++ R/utils.R
@@ -0,0 +1,126 @@
+
+#' Check OTU table
+#' Checks the presence of NA and non-numeric cells
+#' @param otu_tab data frame with the OTUs counts with samples on columns and OTUs in rows.
+#'
+#' @return revised data frame
+#' @export
+#'
+#' @examples
+#' check_OTU_table(otu_tab)
+check_OTU_table <- function(otu_tab) {
+  # Check if all cells are numeric
+  otu_tab <- apply(otu_tab, c(1, 2), as.numeric)
+  if (any(!is.numeric(otu_tab))) {
+    return(stop("Please provide an ASV table with numeric values"))
+  }
+  # Check if there are NA
+  if (any(is.na(otu_tab))) {
+    return(stop("Please provide an ASV table without NA"))
+  }
+  return(otu_tab)
+}
+
+#' Return names
+#' Returns de original names to the OTUs
+#' @param abduced a data frame, containing the significant effects where each row contains a pair of OTU, the effect on the abundance or interaction and the I value supporting the effect.
+#' @param otu_names named character vector with the original otu names in the same order as provided in the original input.
+#'
+#' @return the same input data frame substituting the executing names by the original ones.
+#' @export
+#'
+#' @examples
+#' return_names(infered, otu_names)
+return_names <- function(infered, otu_names) {
+  names(otu_names) <- paste0("s", seq_len(length(otu_names)))
+  infered[, 1:2] <- apply(infered[, 1:2], c(1, 2), function(x) {
+    ifelse(grepl("^s", x), otu_names[x], x)
+  })
+  return(infered)
+}
+
+
+#' Format abduction output
+#' Gives data frame format to the raw abduction output produced by PyGol
+#' @param abduced_raw data frame containing the raw output of PyGol abduction and the compression values.
+#'
+#' @return data frame where each row contains a pair of OTU, the effect on the abundance and the compression supporting the effect.
+#' @export
+#'
+#' @examples
+#' formatOutput(abduced_raw)
+formatOutput <- function(abduced_raw) {
+
+  # Split effect and species
+  eff <- vapply(strsplit(abduced_raw[, 1], split = "\\("), function(x) {
+    x[1]
+  }, FUN.VALUE = character(1))
+  s1 <- vapply(strsplit(abduced_raw[, 1], split = "\\("), function(x) {
+    x[2]
+  }, character(1))
+  s2 <- vapply(strsplit(s1, split = ","), function(x) x[2], character(1))
+
+  # Format species
+  s2 <- gsub(")", "", s2)
+  s1 <- vapply(strsplit(s1, split = ","), function(x) x[1], character(1))
+
+  # Create data.frame
+  df <- data.frame(s1, s2, eff, abduced_raw[, 2], stringsAsFactors = FALSE)
+  colnames(df) <- c("sp1", "sp2", "lnk", "comp")
+  df$comp <- as.numeric(df$comp)
+  return(df)
+}
+
+#' Get comparisons
+#' Produces a list with all the possible comparisons between samples in both directions.
+#' @param n_samples numeric vector with the position of tha samples to compare.
+#'
+#' @return a list with the pairs of samples to compare.
+#' @export
+#'
+#' @examples
+#' get_comparsions(n_samples)
+get_comparsions <- function(n_samples) {
+  # All samples to compare
+  comparisons <- combn(n_samples, 2, NULL, FALSE)
+  # Add the opposite direction
+  other.direction <- lapply(comparisons, function(x) {
+    c(x[2], x[1])
+  })
+  comparisons <- c(comparisons, other.direction)
+  return(comparisons)
+}
+
+#' Load PyGol
+#' Loads PyGol function for abduction using reticulate.
+#' @return
+#' @export
+#'
+#' @examples
+#' load_PyGol()
+load_PyGol <- function() {
+  
+  #Find the python file to import
+  package.location<-  system.file(package = "InfIntE")
+  python.file<- file.path(package.location, "python", "pygol_abduce.py")
+  
+  #Save current work directory 
+  current_wd <- getwd()
+  
+  #Check if there is the compiled so
+  files_python<- list.files(file.path(package.location, "python"))
+  if(!any(grepl("\\.so", files_python))){
+    
+    #Compile using cython
+    setwd(file.path(package.location, "python"))
+    system2("python3",args =  c("generate_shared_object.py", 
+                                "build_ext", "--inplace"))
+    setwd(current_wd)
+  }
+  
+  #Import python functions
+  reticulate::source_python(python.file, envir = globalenv())
+  on.exit(setwd(current_wd))
+}
+
+
